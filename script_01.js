da7bed5bfb9b2adb81775563be38a4a17d2b991a
Septentrion50 <erwan56lecorre@gmail.com>
2021-07-05 11:02:25 +0200

    First commit // Add script_01

---
+++ script_01.js
@@ -0,0 +1,39 @@
+const entrepreneurs = [
+  { first: 'Steve', last: 'Jobs', year: 1955 },
+  { first: 'Oprah', last: 'Winfrey', year: 1954 },
+  { first: 'Bill', last: 'Gates', year: 1955 },
+  { first: 'Sheryl', last: 'Sandberg', year: 1969 },
+  { first: 'Mark', last: 'Zuckerberg', year: 1984 },
+  { first: 'Beyonce', last: 'Knowles', year: 1981 },
+  { first: 'Jeff', last: 'Bezos', year: 1964 },
+  { first: 'Diane', last: 'Hendricks', year: 1947 },
+  { first: 'Elon', last: 'Musk', year: 1971 },
+  { first: 'Marissa', last: 'Mayer', year: 1975 },
+  { first: 'Walt', last: 'Disney', year: 1901 },
+  { first: 'Larry', last: 'Page', year: 1973 },
+  { first: 'Jack', last: 'Dorsey', year: 1976 },
+  { first: 'Evan', last: 'Spiegel', year: 1990 },
+  { first: 'Brian', last: 'Chesky', year: 1981 },
+  { first: 'Travis', last: 'Kalanick', year: 1976 },
+  { first: 'Marc', last: 'Andreessen', year: 1971 },
+  { first: 'Peter', last: 'Thiel', year: 1967 }
+];
+  
+const currentYear = 2021;
+
+const newArray = (inputArray) => {
+  let outputArray = [];
+  inputArray.forEach(person =>{
+    outputArray.push(
+      { firstName: person.first, lastName: person.last, age: currentYear - person.year}
+    );
+  });
+  return outputArray;
+};
+
+const output70 = entrepreneurs.filter(person => person.year >= 1970 && person.year < 1980 );
+
+console.log("Question 1 :");
+console.log(newArray(entrepreneurs));
+console.log("Question 2 :");
+console.log(newArray(output70));
